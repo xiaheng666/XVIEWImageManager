0758a0213f31258cee5846e193eef7d10edbac3b
xiaheng666 <key@xiaheng.net>
2019-01-24 16:27:53 +0800

    xviewimagemanager.h

--- XVIEWImageManager/Classes/XVIEWMultiImageManager/XVIEWMultiImageManager.h
+++ XVIEWImageManager/Classes/XVIEWMultiImageManager/XVIEWMultiImageManager.h
@@ -17,7 +17,7 @@
 
 /**
  *  选择图片上传
- @param  param   data:{@"maxnumber":最大数量}
+ @param  param   data:{@"photoMaxNum":图片最大数量,@"isEdit":是否可编辑 1 可编辑  0不可编辑,@"videoMaxNum":视频最大数量,@"color":主题颜色,@"type":0图片/1视频/2均可}
                 callback:回调
  */
 - (void)choosePics:(NSDictionary *)param;
